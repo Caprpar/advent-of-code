567dd4cf620a3333bb53976914dcdd2106deddcc
Caspar <cappetjatte@gmail.com>
2023-12-06 13:47:41 +0100

    add file reader function

--- advent-of-code/2023/2/main.py
+++ advent-of-code/2023/2/main.py
@@ -1,1 +1,9 @@
+import os
 
+def file_lines_to_list(file):
+    '''Return list which each index represent information from each row from a file'''
+    outputList = []
+    with open(file,"r") as file:
+        for line in file:
+            outputList.append(line.strip())
+    return outputList
